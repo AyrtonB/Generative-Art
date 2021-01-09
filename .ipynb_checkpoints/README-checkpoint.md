4282b885ab601a5950bf90f98c261482e7d5e14a
AyrtonB <AyrtonBourn@Outlook.com>
2021-01-09 23:09:24 +0000

    separated svg helpers

--- .ipynb_checkpoints/README-checkpoint.md
+++
@@ -1,9 +0,0 @@
-# Generative Art
-
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/AyrtonB/Generative-Art/main?urlpath=lab)
-
-<br>
-
-### 01 - Solar System
-
-<img src="img/solar-system/example.png" width="500"/>
--- genart/_nbdev.py
+++ genart/_nbdev.py
@@ -2,26 +2,28 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"vis_graphic": "01-solar-system.ipynb",
-         "draw_img": "01-solar-system.ipynb",
-         "select_new_colour": "01-solar-system.ipynb",
-         "draw_background": "01-solar-system.ipynb",
-         "draw_circle_fill": "01-solar-system.ipynb",
-         "draw_sun": "01-solar-system.ipynb",
-         "draw_orbit_path": "01-solar-system.ipynb",
-         "draw_line_path": "01-solar-system.ipynb",
-         "get_planet_size_center": "01-solar-system.ipynb",
-         "draw_planets": "01-solar-system.ipynb",
-         "draw_border": "01-solar-system.ipynb",
-         "add_noise": "01-solar-system.ipynb",
-         "load_img_data": "02-scenery-dither.ipynb",
-         "rgb_to_bw": "02-scenery-dither.ipynb",
-         "change_resolution": "02-scenery-dither.ipynb",
-         "scale_pixel_intensity": "02-scenery-dither.ipynb",
-         "get_img_negative": "02-scenery-dither.ipynb",
-         "process_img": "02-scenery-dither.ipynb"}
+index = {"draw_img": "01-svg-helpers.ipynb",
+         "vis_graphic": "01-svg-helpers.ipynb",
+         "draw_background": "01-svg-helpers.ipynb",
+         "draw_border": "01-svg-helpers.ipynb",
+         "draw_circle_fill": "01-svg-helpers.ipynb",
+         "draw_sun": "02-solar-system.ipynb",
+         "select_new_colour": "02-solar-system.ipynb",
+         "draw_orbit_path": "02-solar-system.ipynb",
+         "draw_line_path": "02-solar-system.ipynb",
+         "get_planet_size_center": "02-solar-system.ipynb",
+         "draw_planets": "02-solar-system.ipynb",
+         "add_noise": "02-solar-system.ipynb",
+         "draw_solar_system": "02-solar-system.ipynb",
+         "load_img_data": "03-scenery-dither.ipynb",
+         "rgb_to_bw": "03-scenery-dither.ipynb",
+         "change_resolution": "03-scenery-dither.ipynb",
+         "scale_pixel_intensity": "03-scenery-dither.ipynb",
+         "get_img_negative": "03-scenery-dither.ipynb",
+         "process_img": "03-scenery-dither.ipynb"}
 
-modules = ["solar.py",
+modules = ["svg.py",
+           "solar.py",
            "scenery.py"]
 
 doc_url = "https://AyrtonB.github.io/genart/"
